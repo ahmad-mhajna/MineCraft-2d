208a4f8cdfdf161992499619abce60432b6f58ea
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-04-28 20:34:39 +0300

    better imgs font add placeblock done

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let inventory = {
   OakLog: 0,
   grassblock: 0,
   dirt: 0,
-  stone: 0,
+  cobblestone: 0,
 };
 function Worldgen() {
   World.innerText = "";
@@ -28,7 +28,9 @@ function Worldgen() {
     else if (i >= 105 && i < 120) blocks.classList.add("dirt");
     else blocks.classList.add("stone");
     document.querySelectorAll(".block").forEach((block) => {
-      block.addEventListener("click", mineblock);
+      if (block.className.includes("skyblock"))
+        block.addEventListener("click", placeblock);
+      else block.addEventListener("click", mineblock);
     });
   }
   changeBlock([25, 26, 27, 39, 40, 41, 42, 43], "OakLog-leaves");
@@ -38,17 +40,19 @@ function Worldgen() {
 Worldgen();
 function updateCounters() {
   inventoryblocks.forEach((h2) => {
-    if (h2.parentElement.classList[0] === "cobblestone")
-      h2.innerText = inventory["stone"];
-    else h2.innerText = inventory[h2.parentElement.classList[0]];
+    h2.innerText = inventory[h2.parentElement.classList[0]];
   });
 }
 function mineblock(event) {
-  if (blocks[event.target.classList[1]] === selectedTool) {
-    inventory[event.target.classList[1]]++;
-    updateCounters();
+  if (event.target.classList[1] !== "skyblock") {
+    if (blocks[event.target.classList[1]] === selectedTool) {
+      if (event.target.classList[1] === "stone") inventory["cobblestone"]++;
+      else inventory[event.target.classList[1]]++;
+      updateCounters();
 
-    event.target.className = "block skyblock";
+      event.target.className = "block skyblock";
+      event.target.addEventListener("click", placeblock);
+    }
   }
 }
 function pickTool(event) {
@@ -62,9 +66,26 @@ document.querySelectorAll(".tool").forEach((tool) => {
   tool.addEventListener("click", pickTool);
 });
 function changeBlock(id, block) {
-  if (id.length) {
+  if (typeof id === "string") {
+    document.getElementById(id).className = `block ${block}`;
+  } else
     id.forEach((currentId) => {
       document.getElementById(currentId).className = `block ${block}`;
     });
-  } else document.getElementById(id).className = `block ${block}`;
+}
+
+function placeblock(event) {
+  const currentTool = document.querySelector(".selected");
+  let invblock = currentTool.classList[0];
+
+  if (selectedTool === "inventory") {
+    if (event.target.classList[1] === "skyblock") {
+      if (inventory[invblock] > 0) {
+        changeBlock(event.target.id, invblock);
+        inventory[invblock]--;
+        updateCounters();
+        event.target.addEventListener("click", mineblock);
+      }
+    }
+  }
 }
