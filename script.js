380318bd95b61f4f552012f7f8890a5d904d373f
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-04-28 16:28:00 +0300

    basic setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const world = document.querySelector(".world");
+const nav = document.querySelector("nav");
+for (let i = 0; i < 300; i++) {
+  const blocks = document.createElement("div");
+  world.appendChild(blocks);
+}
